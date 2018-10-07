eb5da8506916a05eb93d35a73528df4eac562e80
MaxFish1992 <348756718@qq.com>
2018-10-07 21:41:57 +0800

    数据源配置

---
+++ CustomControls/Button_Diag.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiagramDesigner.CustomControls
+{
+    /// <summary>
+    /// 按照步骤 1a 或 1b 操作，然后执行步骤 2 以在 XAML 文件中使用此自定义控件。
+    ///
+    /// 步骤 1a) 在当前项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根 
+    /// 元素中: 
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:DiagramDesigner.CustomControls"
+    ///
+    ///
+    /// 步骤 1b) 在其他项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根 
+    /// 元素中: 
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:DiagramDesigner.CustomControls;assembly=DiagramDesigner.CustomControls"
+    ///
+    /// 您还需要添加一个从 XAML 文件所在的项目到此项目的项目引用，
+    /// 并重新生成以避免编译错误: 
+    ///
+    ///     在解决方案资源管理器中右击目标项目，然后依次单击
+    ///     “添加引用”->“项目”->[浏览查找并选择此项目]
+    ///
+    ///
+    /// 步骤 2)
+    /// 继续操作并在 XAML 文件中使用控件。
+    ///
+    ///     <MyNamespace:Button_Diag/>
+    ///
+    /// </summary>
+    public class Button_Diag : Button
+    {
+        static Button_Diag()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Button_Diag), new FrameworkPropertyMetadata(typeof(Button_Diag)));
+        }
+        [DisplayName("文字")]
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(Button_Diag), new PropertyMetadata("Button_Diag"));
+
+        [DisplayName("文字颜色")]
+        public SolidColorBrush TextColor
+        {
+            get { return (SolidColorBrush)GetValue(TextColorProperty); }
+            set { SetValue(TextColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TextColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextColorProperty =
+            DependencyProperty.Register("TextColor", typeof(SolidColorBrush), typeof(Button_Diag), new PropertyMetadata(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000"))));
+
+        [DisplayName("文字大小")]
+        public int TextSize
+        {
+            get { return (int)GetValue(TextSizeProperty); }
+            set { SetValue(TextSizeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TextSize.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextSizeProperty =
+            DependencyProperty.Register("TextSize", typeof(int), typeof(Button_Diag), new PropertyMetadata(12));
+
+
+        public SolidColorBrush BackColor
+        {
+            get { return (SolidColorBrush)GetValue(BackColorProperty); }
+            set { SetValue(BackColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for BackColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BackColorProperty =
+            DependencyProperty.Register("BackColor", typeof(SolidColorBrush), typeof(Button_Diag), new PropertyMetadata(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FBFBFB"))));
+
+
+    }
+}
